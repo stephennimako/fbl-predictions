a8aa452e5a35919ec6688f720d79f2dcc29c5e1b
stephennimako <stephennimako.sn@gmail.com>
2014-10-23 21:56:17 +0100

    increasing capybara default wait time to reduce flakiness of tests

--- features/support/env.rb
+++ features/support/env.rb
@@ -3,6 +3,7 @@ Bundler.require :test
 Capybara.app_host = "http://localhost:9291"
 Capybara.run_server = false
 Capybara.default_driver = :poltergeist
+Capybara.default_wait_time = 5
 
 WebMock.allow_net_connect!
 
