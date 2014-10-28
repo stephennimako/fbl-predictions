df2b8e4cadaf1a395f963c6fef558146375e4a6d
stephennimako <stephennimako.sn@gmail.com>
2014-10-28 20:11:28 +0000

    added poltergeist timeout

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,5 +1,9 @@
 Bundler.require :test
 
+Capybara.register_driver :poltergeist do |app|
+  Capybara::Poltergeist::Driver.new(app, {js_errors: false, timeout: 5, phantomjs_options: ['--load-images=no']})
+end
+
 Capybara.app_host = "http://localhost:9291"
 Capybara.run_server = false
 Capybara.default_driver = :poltergeist
